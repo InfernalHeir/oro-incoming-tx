cee5000f463388b96624346f987d0a213ea6b861
Bhupendra Bisht <bhupendrabisht61049@gmail.com>
2021-09-20 15:08:52 +0530

    testnet to mainnet

--- constants/index.ts
+++ constants/index.ts
@@ -1,12 +1,12 @@
-export const NETWORK: string = "granadanet";
+export const NETWORK: string = "mainnet";
 
 export interface Tokens {
   [tokenId: number]: string;
 }
 
 export const tokens: Tokens = {
-  0: "KT1G16QAxCmEF3zjvnz817tfL3k6AYg2j8nx",
-  1: "KT1AiJpnvsR8s1KrajAwAzELYfYKpiqknRfm",
+  0: "KT1S6t5PrHXnozytDU3vYdajmsenoBNYY8WJ",
+  1: "KT1EqhKGcu9nztF5p9qa4c3cYVqVewQrJpi2",
 };
 
 // bcd base url
@@ -14,7 +14,7 @@ export const BASEURL: string = "https://api.better-call.dev";
 
 export const TEZOS_MAIN: string = "http://tezosapi.oropocket.com";
 
-export const phpServerEndpoint: string = "https://dev.my.oropocket.com/";
+export const phpServerEndpoint: string = "https://my.oropocket.com/";
 
 export const phpAuth: string = "607806ddf20e8483b289cb3c245ec52f";
 
